ebafeb644294f834f1b14724151d758bbf16152c
Saikat Chakraborty <saikatgpae@gmail.com>
2022-11-23 14:39:30 +0530

    Add first-post page.

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const FirstPost= () =>{
+    return (
+        <>
+            <h4><Link href="/">Home</Link></h4>
+            <h1>hi</h1>
+        </>
+    );
+
+  };
+
+export default FirstPost;
